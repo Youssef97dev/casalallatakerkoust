ba7771aa9d0b628e3254217bd58188b292a6da43
Youssef97dev <youssefmoussafi97@gmail.com>
2025-10-30 12:45:39 +0100

    add contact page

---
+++ src/app/contact/components/Content.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+function Content() {
+  return (
+    <section
+      id="contact"
+      className="w-full grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-5 px-3 lg:px-40 pt-24"
+    >
+      <div className="w-full flex flex-col justify-start items-center gap-5">
+        <h1 className="text-[35px] leading-[42px] font-canela text-casa_black">
+          Reserve online
+        </h1>
+        <Link
+          href="/booking"
+          className={`tracking-[0.2em] mt-2 uppercase text-[11px] font-medium  py-3 px-5 transition-all duration-300 ease-in-out border-2 border-casa_scarlet text-casa_scarlet rounded-full hover:bg-casa_scarlet hover:text-casa_background`}
+        >
+          Book Here
+        </Link>
+        <p className="font-light text-[15px] leading-[25.5px] text-casa_black">
+          Customer Service |{" "}
+          <span className="text-casa_scarlet underline underline-offset-4">
+            +212 675-480103
+          </span>
+        </p>
+        <p className="font-light text-[15px] leading-[25.5px] text-casa_black">
+          Email Address |{" "}
+          <span className="text-casa_scarlet underline underline-offset-4">
+            reservations@casalallatakerkoust.com
+          </span>
+        </p>
+      </div>
+      <div className="w-full flex flex-col justify-start items-center gap-5">
+        <h1 className="text-[35px] leading-[42px] font-canela text-casa_black">
+          Our Location
+        </h1>
+        <h2 className="text-[15px] leading-[25.5px] font-light text-casa_scarlet underline underline-offset-4">
+          Lalla Takerkoust, Marrakech 40000 Morocco
+        </h2>
+        <p className="text-[15px] leading-[25.5px] font-light text-casa_black text-center">
+          Open Thursday to Sunday <br />
+          Opening hours : 1:00 PM to 10:00 PM
+        </p>
+        <Link href="https://maps.app.goo.gl/W2KBNPAQ7UNFHhJx5">
+          <Image
+            src="https://casalallatakerkoust.com/Plant-1.png"
+            alt="casa lalla takerkoust location"
+            width={500}
+            height={500}
+            className="object-cover w-[400px]"
+          />
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Content;
---
+++ src/app/contact/components/Hero.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div id="hero" className="relative w-full h-screen">
+      <div className="w-full h-full hidden lg:block">
+        <Image
+          src="/images/casalalla-takerkoust-agafay-large-2.jpg"
+          alt="casa lalla takerkoust, agafay vibes"
+          width={1500}
+          height={1500}
+          className={`w-full h-full object-cover`}
+        />
+      </div>
+      <div className="w-full h-full block lg:hidden">
+        <Image
+          src="https://cdn.pixabay.com/photo/2025/03/28/11/00/marrakech-9498605_1280.jpg"
+          alt="casa lalla takerkoust, agafay vibes"
+          width={1500}
+          height={1500}
+          className={`w-full h-full object-cover`}
+        />
+      </div>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-40 hidden lg:block"></div>
+
+      <div className="absolute inset-0 w-full flex justify-center text-center items-center font-canela text-white text-[38px] leading-[40px] lg:text-[40px] lg:leading-[60px] px-2 lg:px-0">
+        <h1>Contact us</h1>
+      </div>
+
+      <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 text-white z-40">
+        <a href="#contact">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 animate-bounce"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 15l7 7 7-7"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
