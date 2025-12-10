9facb58583cba898e67e5cd2d8c030f50fb33ac9
Youssef97dev <youssefmoussafi97@gmail.com>
2025-12-10 11:04:39 +0100

    add day pass page

---
+++ src/app/day-pass/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+const Intro = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-4 pt-28 px-4 lg:px-56">
+      <h1 className="text-[35px] leading-[45px] text-center tracking-[2px] text-salama_text font-canela text-casa_scarlet mb-2 px-10">{`Day Pass & Lunch`}</h1>
+      <p className="text-[14px] lg:text-[12px] lg:leading-[23px] text-center font-light lg:font-normal tracking-wider leading-[28px] px-4 2xl:px-12 text-salama_text">{`Spend a perfect day at Casa Lalla Takerkoust.`}</p>
+      <p className="text-[14px] lg:text-[12px] lg:leading-[23px] text-center font-light lg:font-normal tracking-wider leading-[28px] px-4 2xl:px-12 text-salama_text">{`Relax in our peaceful gardens, enjoy our panoramic view overlooking Lake Lalla Takerkoust, and savour a delicious farm-to-table lunch prepared with fresh local ingredients.`}</p>
+      <p className="text-[14px] lg:text-[12px] lg:leading-[23px] text-center font-light lg:font-normal tracking-wider leading-[28px] px-4 2xl:px-12 text-salama_text">{`Open every day from 13:00 PM to 17:00 PM, it’s the perfect place to enjoy lunch at any time.`}</p>
+      <Link
+        href="/booking"
+        className={`tracking-[0.2em] mt-2 uppercase text-[11px] font-medium  py-3 px-5 transition-all duration-300 ease-in-out border-2 border-casa_scarlet text-casa_scarlet rounded-full hover:bg-casa_scarlet hover:text-casa_background`}
+      >
+        Reservations
+      </Link>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/app/fr/day-pass/components/Gallery.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+
+const GalleryImage = [
+  {
+    image: "/images/lake-15.jpg",
+    class: "object-center",
+  },
+  {
+    image: "/images/lake-24.jpg",
+    class: "object-bottom",
+  },
+  {
+    image: "/images/casalalla-takerkoust-agafay-vibes-gallery-3.jpg",
+    class: "object-bottom",
+  },
+  {
+    image: "/images/casalalla-restaurant-agafay.jpeg",
+    class: "object-center",
+  },
+];
+
+const Gallery = () => {
+  return (
+    <section className="px-5 md:px-14 py-12 space-y-8 pt-28">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4  gap-5">
+        {GalleryImage.map((category, index) => (
+          <div key={index} className="flex flex-col">
+            {/* Image */}
+            <div className="relative w-full aspect-square ">
+              <Image
+                src={category.image}
+                alt="Gallery Pure House On The Lake, Agafay, Agavay Vibes, Lalla Takerkoust"
+                fill
+                className={`object-cover aspect-square h-full ${category.class}`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
