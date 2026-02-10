dc2f449a9d0cb4e0df343ff0d1eb7a8f2cb50bb3
Youssef97dev <youssefmoussafi97@gmail.com>
2026-02-10 17:36:22 +0100

    change meta data in reservation page

--- src/app/reservation/page.jsx
+++ src/app/reservation/page.jsx
@@ -2,37 +2,9 @@ import React from "react";
 import Loader from "./components/Loader";
 
 export const metadata = {
-  title: {
-    default: "Casa Lalla Takerkoust",
-    template: "%s | Reserve a Table",
-  },
+  title: "Agafay Wedding Venue & Restaurant | Ibiza Style Events | Casa Lalla",
   description:
-    "Reserve your table at Casa Lalla Takerkoust, a unique dining destination near Marrakech overlooking Lalla Takerkoust Lake and the Atlas Mountains. Book online and enjoy an unforgettable culinary experience blending Moroccan and international cuisine.",
-  keywords: [
-    "book restaurant marrakech",
-    "table reservation marrakech",
-    "restaurant booking lalla takerkoust",
-    "casa lalla takerkoust booking",
-    "lake view restaurant reservation",
-  ],
-  openGraph: {
-    title: "Reserve a Table | Casa Lalla Takerkoust",
-    description:
-      "Rserve your table at Casa Lalla Takerkoust and enjoy Moroccan and international cuisine with breathtaking views of Lalla Takerkoust Lake and the Atlas Mountains.",
-    url: "https://casalallatakerkoust.com/reservation",
-    siteName: "Casa Lalla Takerkoust",
-    images: [
-      {
-        url: "https://casalallatakerkoust.com/casalalla-takerkoust-agafay-vibes.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Reserve a table at Casa Lalla Takerkoust",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  metadataBase: new URL("https://casalallatakerkoust.com"),
+    "Book your private event or Agafay wedding at Casa Lalla. Experience a top Moroccan restaurant with a chic Ibiza Mykonos style atmosphere by the lake",
 };
 
 const page = () => {
