48df819bbbaa3ddf826852cfb6ab965cb0587666
Youssef97dev <youssefmoussafi97@gmail.com>
2026-04-02 15:59:32 +0100

    add 3 articles

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Loader from "./components/Loader";
+
+export const metadata = {
+  title: "Lalla Takerkoust Blog| Casa Lalla | Ibiza Style & Agafay Vibes",
+  description: "",
+};
+
+const page = () => {
+  return <Loader />;
+};
+
+export default page;
