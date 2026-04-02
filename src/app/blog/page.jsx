f8fae052e768fa97f2fa1360a9bb6a4256d8b0b9
Youssef97dev <youssefmoussafi97@gmail.com>
2026-04-02 16:00:48 +0100

    add description to main page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,7 +3,8 @@ import Loader from "./components/Loader";
 
 export const metadata = {
   title: "Lalla Takerkoust Blog| Casa Lalla | Ibiza Style & Agafay Vibes",
-  description: "",
+  description:
+    "Discover travel tips, adventure guides, and local secrets for exploring Lalla Takerkoust, the Agafay Desert, and Marrakech on the Casa Lalla Takerkoust blog.",
 };
 
 const page = () => {
