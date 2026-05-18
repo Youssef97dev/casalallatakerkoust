b87b4427a498df1e76af7b36107766ed719a976b
Youssef97dev <youssefmoussafi97@gmail.com>
2026-05-18 09:54:58 +0100

    update gallery

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,6 +5,13 @@ export const metadata = {
   title: "Photo Gallery | Casa Lalla Takerkoust | Ibiza Style Vibe",
   description:
     "Explore the Casa Lalla Takerkoust gallery. Discover our Ibiza-style restaurant and agafay vibes near Marrakech featuring stunning lake views and a vibrant atmosphere.",
+  openGraph: {
+    title: "Photo Gallery | Casa Lalla Takerkoust | Ibiza Style Vibe",
+    description: "Visual journey through our oasis in the agafay desert.",
+    images: [
+      "/agafay-takerkoust/romantic-night-dining-ambiance-casa-lalla-takerkoust.webp",
+    ], // Add a preview image path here
+  },
 };
 
 const page = () => {
